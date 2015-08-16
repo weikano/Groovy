6a16f40ff7035f4e718b85fa80da2b7035dcdc76
weikano <weikano@163.com>
2015-08-16 18:18:53 +0800

    hah

--- src/programming_groovy/chapter7collections/Find.groovy
+++ src/programming_groovy/chapter7collections/Find.groovy
@@ -1,2 +1,16 @@
 list = [1,2,3,4,5]
 println list.findAll{it>3}
+println list.findAll{it>3}.sum()
+//carryOver 初始值为0，carryOver的值会进入下一个闭包
+println list.inject {carryOver ,item ->
+    println "carryOver $carryOver and item $item"
+    carryOver +=1
+}
+
+println list.join(" ")
+
+def numbers(a,b,c,d,e){
+    println "$a $b $c $d $e"
+}
+//*将集合展开
+numbers(*list)
